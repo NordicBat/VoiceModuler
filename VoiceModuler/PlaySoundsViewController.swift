3d727655c74d8735c0b0aa57cd0b5e1c49d93824
Gabriel Meira <gabriel.meira.25@gmail.com>
2018-10-14 19:02:35 +0200

    First commit with sourcetree. Taking the udacity "intro to ios dev" course. Just for study purposes.

---
+++ VoiceModuler/PlaySoundsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PlaySoundsViewController.swift
+//  VoiceModuler
+//
+//  Created by Gabriel de Freitas Meira on 14/10/2018.
+//  Copyright © 2018 Test. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+
+    var recordedAudioURL: URL!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ VoiceModuler/RecSoundsViewController.swift
@@ -0,0 +1,88 @@
+//
+//  RecSoundsViewController.swift
+//  VoiceModuler
+//
+//  Created by Gabriel de Freitas Meira on 07/10/2018.
+//  Copyright © 2018 Test. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class RecSoundsViewController: UIViewController, AVAudioRecorderDelegate {
+    
+    var audioRecorder: AVAudioRecorder!
+
+    @IBOutlet weak var recordingLabel: UILabel!
+    
+    @IBOutlet weak var recordButton: UIButton!
+    
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        stopRecordingButton.isEnabled = false
+    }
+
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear")
+    }
+
+    @IBAction func recordAudio(_ sender: Any) {
+        print("button pressed")
+        recordingLabel.text = "Recording in progress"
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
+        
+//      cria o diretorio
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true) [0] as String
+//      o nome do arquivo
+        let recordingName = "RecordAudio.wav"
+
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+        print(filePath!)
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSession.Category.playAndRecord, mode: AVAudioSession.Mode.default)
+        
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.delegate = self
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+        
+    }
+    @IBAction func stopRecording(_ sender: Any) {
+        print("Stop Recording")
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
+        recordingLabel.text = "Tap to Record"
+        
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
+    }
+    
+//    verificando se o audio foi gravado
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if flag {   
+            performSegue(withIdentifier: "stopRecording", sender: audioRecorder.url)
+        } else {
+            print("meh")
+        }
+    }
+
+
+//  informar para onde sera enviado a acao
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "stopRecording" {
+            let playSoundsVC = segue.destination as! PlaySoundsViewController
+            let recordedAudioURL = sender as! URL
+            playSoundsVC.recordedAudioURL = recordedAudioURL
+        }
+    }
+}
+
